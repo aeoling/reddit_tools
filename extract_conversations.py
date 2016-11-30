38ffb2df4e1e96c7f9413127523d85066e5ee963
Igor Shalyminov <ishalyminov@gmail.com>
2016-11-30 15:37:04 +0000

    one-file comment chaining implemented

--- extract_conversations.py
+++ extract_conversations.py
@@ -1,30 +1,44 @@
 import json
-from sys import argv
+from sys import argv, stdout
 from collections import defaultdict
-from codecs import getreader
+from codecs import getreader, getwriter
+import logging
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
 
 
-def main(in_file_name):
-    comment_ids = defaultdict(lambda: [])
+def build_comment_chains(in_file_name):
+    comment_chains = defaultdict(lambda: [])
     conversation_roots = {}
+
     with getreader('utf-8')(open(in_file_name)) as reddit_in:
         for line in reddit_in:
             comment_json = json.loads(line)
-            comment_id, parent_id = comment_json.get('id', None), comment_json.get('parent_id', None)
-            if not parent_id:
-                comment_ids[comment_id].append(comment_id)
+            comment_id, parent_id = (
+                comment_json.get('id', None),
+                comment_json.get('parent_id', None)
+            )
+            # we have the root of the comment chain - probably, top level comment
+            if not parent_id or parent_id.partition('_')[2] not in conversation_roots:
+                comment_chains[comment_id].append(comment_json['body'])
                 conversation_roots[comment_id] = comment_id
             else:
                 parent_id = parent_id.partition('_')[2]
-                if not parent_id in conversation_roots:
-                    continue
                 conversation_roots[comment_id] = conversation_roots[parent_id]
-                comment_ids[conversation_roots[comment_id]].append(comment_id)
-    return comment_ids
+                comment_chains[conversation_roots[comment_id]].append(comment_json['body'])
+    return comment_chains
 
 
 if __name__ == '__main__':
     if len(argv) < 2:
         print 'Usage: extract_conversations.py <input filename>'
         exit()
-    print main(argv[1]).values()[:5]
+    comment_chains = build_comment_chains(argv[1])
+    logger.info('#chains found: {}'.format(len(comment_chains)))
+
+    with getwriter('utf-8')(stdout) as OUTPUT_WRITER:
+        for chain_id, chain in comment_chains.iteritems():
+            print >>OUTPUT_WRITER, u'\n'.join(chain)
+
