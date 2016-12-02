d5bedcf3559ea2ff0130211dadb024380436a1b0
Igor Shalyminov <ishalyminov@gmail.com>
2016-12-02 18:26:27 +0000

    updates

--- filter_size.py
+++ filter_size.py
@@ -1,16 +1,21 @@
 import json
 import sys
 
-def getSubreddits(input_file):
+MAX_UTTERANCE_SIZE = 60
 
-    with open(input_file, 'r') as f:
-        for line in f:
-            json_line = json.loads(line)
-            print json_line, '\n'
-            body = json_line.get('body', None)
 
-            if len(body.split()) < 60:
-                print line
+def getSubreddits(input_stream, in_max_len):
+    for line in input_stream:
+        json_line = json.loads(line)
+        body = json_line.get('body', None)
+
+        if len(body.split()) < in_max_len:
+            print line.strip()
+
 
 if __name__ == '__main__':
-    getSubreddits(sys.argv[1])
+    maxlen = int(sys.argv[1]) \
+        if 1 < len(sys.argv) \
+        else MAX_UTTERANCE_SIZE
+    getSubreddits(sys.stdin, maxlen)
+
--- process_corpus.sh
+++ process_corpus.sh
@@ -1,6 +1,5 @@
 for file in `find ../../reddit_data -type f`; do
-    echo "Processing $file"
-    out_file_name=reddit_data_chains/$(basename $file)
+    out_file_name=reddit_filtered/$(basename $file)
     log_file_name=${out_file_name}.log
-    python extract_conversations.py < $file 1>$out_file_name 2>$log_file_name
+    nohup python filter_size.py < $file 1>$out_file_name 2>$log_file_name &
 done
