2266334af46a4d43e7fa126f196f0fa4b22fa2cf
Igor Shalyminov <ishalyminov@gmail.com>
2016-12-02 22:58:43 +0000

    chaining parallelized

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # reddit_tools
 Tools for processing Reddit conversations data
 
-Usage: `python extract_conversations < <input_file> > <output_file>`
+Usage:
+
+1. `python process_corpus.py filter <raw texts root> > <result texts root>`
+2. `python process_corpus.py chain <filtered texts root> <result root>`
--- process_corpus.py
+++ process_corpus.py
@@ -1,9 +1,10 @@
-from sys import argv
 from os import path, walk
 import logging
+from argparse import ArgumentParser
 
 from task_list import add_task, execute_tasks, tasks
-from filter_size import getSubreddits, MAX_UTTERANCE_SIZE
+from filter_size import getSubreddits
+from extract_conversations import get_comment_chains, build_comment_chains
 
 DEFAULT_TASKS_NUMBER = 64
 
@@ -17,36 +18,59 @@ def filter_file_callback(in_params):
         input_file, output_file, max_len = in_params
         logger.info('Processing file {}'.format(input_file))
         with open(input_file) as reddit_in, open(output_file, 'w') as reddit_out:
-            getSubreddits(reddit_in, reddit_out, max_len)
+            getSubreddits(reddit_in, reddit_out)
         return 0
     except:
         return 1
 
 
-def collect_tasks(in_src_root, in_dst_root):
+def build_chains_callback(in_params):
+    try:
+        input_file, output_file = in_params
+        logger.info('Processing file {}'.format(input_file))
+        with open(input_file) as reddit_in, open(output_file, 'w') as reddit_out:
+            comments_tree_root = build_comment_chains(reddit_in)
+            for comment_chain in get_comment_chains(comments_tree_root):
+                print >>reddit_out, u'\n'.join(
+                    ['\t'.join(node_content) for node_content in comment_chain]
+                )
+                print >>reddit_out, '#' * 60
+        return 0
+    except:
+        return 1
+
+
+def collect_tasks(in_src_root, in_dst_root, **kwargs):
     for root, dirs, files in walk(in_src_root):
         for filename in files:
             if not filename.startswith('RC'):
                 continue
             full_filename = path.join(root, filename)
             result_filename = path.join(in_dst_root, filename)
-            add_task((full_filename, result_filename, MAX_UTTERANCE_SIZE))
+            add_task((full_filename, result_filename, kwargs))
 
 
-def main(in_text_root, in_result_folder, in_tasks_number):
+def build_argument_parser():
+    parser = ArgumentParser()
+    parser.add_argument('command')
+    parser.add_argument('src_root')
+    parser.add_argument('result_root')
+    parser.add_argument('--jobs', type=int, default=DEFAULT_TASKS_NUMBER)
+    return parser
+
+
+def main(in_text_root, in_result_folder, in_callback, in_tasks_number):
     collect_tasks(in_text_root, in_result_folder)
     logger.info('got {} tasks'.format(len(tasks)))
     if 1< in_tasks_number:
-        retcodes = execute_tasks(filter_file_callback, in_tasks_number)
+        retcodes = execute_tasks(in_callback, in_tasks_number)
     else:
-        retcodes = [filter_file_callback(task) for task in tasks]
+        retcodes = [in_callback(task) for task in tasks]
     assert sum(retcodes) == 0, 'Some tasks failed!'
 
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        print 'Usage: {} <root folder> <result folder> [tasks_number=64]'.format(argv[0])
-        exit()
-    root_folder, result_folder = argv[1:3]
-    tasks_number = DEFAULT_TASKS_NUMBER if len(argv) < 4 else int(argv[3])
-    main(root_folder, result_folder, tasks_number)
+    parser = build_argument_parser()
+    args = parser.parse_args()
+    callback = locals()[args.command]
+    main(args.src_root, args.result_root, callback, args.jobs)
