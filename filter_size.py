722a7e041e95bb8378959e37067617eaceb6166a
Igor Shalyminov <ishalyminov@gmail.com>
2016-12-02 19:01:37 +0000

    preprocessing parallelized

--- filter_size.py
+++ filter_size.py
@@ -4,18 +4,17 @@ import sys
 MAX_UTTERANCE_SIZE = 60
 
 
-def getSubreddits(input_stream, in_max_len):
+def getSubreddits(input_stream, output_stream, in_max_len):
     for line in input_stream:
         json_line = json.loads(line)
         body = json_line.get('body', None)
 
         if len(body.split()) < in_max_len:
-            print line.strip()
+            print >>output_stream, line.strip()
 
 
 if __name__ == '__main__':
     maxlen = int(sys.argv[1]) \
         if 1 < len(sys.argv) \
         else MAX_UTTERANCE_SIZE
-    getSubreddits(sys.stdin, maxlen)
-
+    getSubreddits(sys.stdin, sys.stdout, maxlen)
--- process_corpus.py
+++ process_corpus.py
@@ -1,13 +1,46 @@
 from sys import argv
+from os import path
+import logging
+
+from task_list import add_task, execute_tasks, tasks
+from filter_size import getSubreddits, MAX_UTTERANCE_SIZE
+
+DEFAULT_TASKS_NUMBER = 64
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
 
 
-def main(in_texts_root, in_result_folder):
-    pass
+def filter_file_callback(in_params):
+    try:
+        input_file, output_file, max_len = in_params
+        with open(input_file) as reddit_in, open(output_file, 'w') as reddit_out:
+            getSubreddits(reddit_in, reddit_out, max_len)
+        return 0
+    except:
+        return 1
+
+
+def collect_tasks(in_src_root, in_dst_root):
+    for root, dirs, files in path.walk(in_src_root):
+        for filename in files:
+            full_filename = path.join(in_src_root, filename)
+            result_filename = path.join(in_dst_root, filename)
+            add_task((full_filename, result_filename, MAX_UTTERANCE_SIZE))
+
+
+def main(in_text_root, in_result_folder, in_tasks_number):
+    collect_tasks(in_text_root, in_result_folder)
+    logger.info('got {} tasks'.format(len(tasks)))
+    retcodes = execute_tasks(filter_file_callback, in_tasks_number)
+    assert sum(retcodes) == 0, 'Some tasks failed!'
 
 
 if __name__ == '__main__':
     if len(argv) < 2:
-        print 'Usage: {} <root folder> <result folder>'
+        print 'Usage: {} <root folder> <result folder> [tasks_number=64]'
         exit()
     root_folder, result_folder = argv[1:3]
-    main(root_folder, result_folder)
+    tasks_number = DEFAULT_TASKS_NUMBER if len(argv) < 3 else int(argv[3])
+    main(root_folder, result_folder, tasks_number)
